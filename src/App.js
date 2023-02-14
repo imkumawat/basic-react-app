9f843a9615c73003cddca1c4fa939aaeef4ea56e
Manoj Kumawat <imkumawatmanoj@gmail.com>
2023-02-14 14:53:01 +0530

    a

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit Please <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Manoj Kumawat</p>
         <a
           className='App-link'
           href='https://reactjs.org'
